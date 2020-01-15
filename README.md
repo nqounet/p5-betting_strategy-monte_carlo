fb8e2a846c0307f0c72b0310a46d154341c483ca
nqounet <mail@nqou.net>
2020-01-16 04:01:56 +0900

    ohai

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+BettingStrategy::MonteCarlo - It's new $module
+
+# SYNOPSIS
+
+    use BettingStrategy::MonteCarlo;
+
+# DESCRIPTION
+
+BettingStrategy::MonteCarlo is ...
+
+# LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nqounet <mail@nqou.net>
---
+++ lib/BettingStrategy/MonteCarlo.pm
@@ -0,0 +1,39 @@
+package BettingStrategy::MonteCarlo;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+BettingStrategy::MonteCarlo - It's new $module
+
+=head1 SYNOPSIS
+
+    use BettingStrategy::MonteCarlo;
+
+=head1 DESCRIPTION
+
+BettingStrategy::MonteCarlo is ...
+
+=head1 LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nqounet E<lt>mail@nqou.netE<gt>
+
+=cut
+
